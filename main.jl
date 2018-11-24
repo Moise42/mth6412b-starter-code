1a9451357f0f8b3ccd6d7075b34be90370128de3
Dan Judkiewicz <dajud@labos.polymtl.ca>
2018-11-24 18:49:26 -0500

    algo rst dans une fonction séparée du main

--- main.jl
+++ main.jl
@@ -5,6 +5,7 @@ include("./projet/read_stsp.jl")
 include("./projet/graph.jl")
 include("./projet/kruskal.jl")
 include("./projet/prim.jl")
+include("./projet/rst.jl")
 
 graph_file = "bays29"
 graph_path = "./instances/stsp/"*graph_file*".tsp"
@@ -30,22 +31,23 @@ buildMST!(prim,prim.nodes[1])
 
 ## RST algorithm
 
-circuit_nodes = prim.order_of_visit
-n = length(circuit_nodes)
-T = typeof(circuit_nodes[1].data)
-circuit_edges = Vector{Edge{T}}()
-circuit_weight = 0;
-for k = 1:n-1
-    e_idx = findall(x-> ( isequal(x.node1,circuit_nodes[k]) && isequal(x.node2,circuit_nodes[k+1])
-    || isequal(x.node1,circuit_nodes[k+1]) && isequal(x.node2,circuit_nodes[k])  ), E)[1]
-    push!(circuit_edges, E[e_idx]);
-    circuit_weight += E[e_idx].weight;
-end
-e_idx = findall(x-> ( isequal(x.node1,circuit_nodes[1]) && isequal(x.node2,circuit_nodes[n])
-|| isequal(x.node1,circuit_nodes[n]) && isequal(x.node2,circuit_nodes[1])  ), E)[1]
-push!(circuit_edges, E[e_idx]);
-circuit_weight += E[e_idx].weight;
+#circuit_nodes = prim.order_of_visit
+#n = length(circuit_nodes)
+#T = typeof(circuit_nodes[1].data)
+#circuit_edges = Vector{Edge{T}}()
+#circuit_weight = 0;
+#for k = 1:n-1
+#    e_idx = findall(x-> ( isequal(x.node1,circuit_nodes[k]) && isequal(x.node2,circuit_nodes[k+1])
+#    || isequal(x.node1,circuit_nodes[k+1]) && isequal(x.node2,circuit_nodes[k])  ), E)[1]
+#    push!(circuit_edges, E[e_idx]);
+#    circuit_weight += E[e_idx].weight;
+#end
+#e_idx = findall(x-> ( isequal(x.node1,circuit_nodes[1]) && isequal(x.node2,circuit_nodes[n])
+#|| isequal(x.node1,circuit_nodes[n]) && isequal(x.node2,circuit_nodes[1])  ), E)[1]
+#push!(circuit_edges, E[e_idx]);
+#circuit_weight += E[e_idx].weight;
 
+circuit_edges = rst!(prim)
 
 
 
---
+++ projet/rst.jl
@@ -0,0 +1,22 @@
+include("./prim.jl")
+
+
+
+function rst!(prim::Prim{T}) where T
+    circuit_nodes = prim.order_of_visit
+    n = length(circuit_nodes)
+    T = typeof(circuit_nodes[1].data)
+    circuit_edges = Vector{Edge{T}}()
+    circuit_weight = 0;
+    for k = 1:n-1
+        e_idx = findall(x-> ( isequal(x.node1,circuit_nodes[k]) && isequal(x.node2,circuit_nodes[k+1])
+        || isequal(x.node1,circuit_nodes[k+1]) && isequal(x.node2,circuit_nodes[k])  ), E)[1]
+        push!(circuit_edges, E[e_idx]);
+        circuit_weight += E[e_idx].weight;
+    end
+    e_idx = findall(x-> ( isequal(x.node1,circuit_nodes[1]) && isequal(x.node2,circuit_nodes[n])
+    || isequal(x.node1,circuit_nodes[n]) && isequal(x.node2,circuit_nodes[1])  ), E)[1]
+    push!(circuit_edges, E[e_idx]);
+    circuit_weight += E[e_idx].weight;
+    return circuit_edges
+end
